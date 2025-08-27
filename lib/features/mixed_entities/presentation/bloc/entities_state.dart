f62828486205e2d9a35a4dce9996850dc9bc6706
RomanTuras <salden.com@gmail.com>
2025-08-27 20:10:39 +0300

    Remove dependency

--- lib/features/mixed_entities/presentation/bloc/entities_state.dart
+++ lib/features/mixed_entities/presentation/bloc/entities_state.dart
@@ -1,25 +1,29 @@
 import 'package:recipes/features/mixed_entities/domain/models/mixed_entity.dart';
-import 'package:equatable/equatable.dart';
 
 enum EntitiesStatus { initial, loading, success, error }
 
-class EntitiesState extends Equatable {
+class EntitiesState {
   final EntitiesStatus status;
   final String? errorMessage;
 
   final List<MixedEntity> mixedEntities;
 
-  const EntitiesState._({required this.status, this.errorMessage, required this.mixedEntities,});
+  const EntitiesState._({
+    required this.status,
+    this.errorMessage,
+    required this.mixedEntities,
+  });
 
-  factory EntitiesState.initial() => EntitiesState._(status: EntitiesStatus.initial, mixedEntities: []);
+  factory EntitiesState.initial() =>
+      EntitiesState._(status: EntitiesStatus.initial, mixedEntities: []);
 
-  EntitiesState copyWith({EntitiesStatus? status, String? errorMessage, List<MixedEntity>? mixedEntities}) =>
-      EntitiesState._(
-          status: status ?? this.status,
-          errorMessage: errorMessage ?? this.errorMessage,
-          mixedEntities: mixedEntities ?? this.mixedEntities
-      );
-
-  @override
-  List<Object?> get props => [status, errorMessage, mixedEntities];
+  EntitiesState copyWith({
+    EntitiesStatus? status,
+    String? errorMessage,
+    List<MixedEntity>? mixedEntities,
+  }) => EntitiesState._(
+    status: status ?? this.status,
+    errorMessage: errorMessage ?? this.errorMessage,
+    mixedEntities: mixedEntities ?? this.mixedEntities,
+  );
 }
